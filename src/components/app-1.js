ff7e085d68ca544e326bc778d4e4a4bd6d993578
sizzle01 <talktoaliyu@gmail.com>
2023-01-10 07:09:54 -0500

    I added the upvote function to product

--- src/components/app-1.js
+++ src/components/app-1.js
@@ -4,37 +4,65 @@ import './seed';
 
 
 
-class ProductList extends React.Component {
+/* eslint-disable no-param-reassign, operator-assignment */
 
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: window.Seed.products });
+  }
+
+  // Inside `ProductList`
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
-    }
-    
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
   render() {
-    const products = window.Seed.products.sort((a, b) => (
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
     ));
-    
-    const productComponents = window.Seed.products.map((product) => (
+    const productComponents = products.map((product) => (
       <Product
-      key={'product-' + product.id}
-      id={product.id}
-      title={product.title}
-      description={product.description}
-      url={product.url}
-      votes={product.votes}
-      submitterAvatarUrl={product.submitterAvatarUrl}
-      productImageUrl={product.productImageUrl}
-      onVote={this.handleProductUpVote}
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
-      ));
-      return (
-        <div className='ui unstackable items'>
+    ));
+    return (
+      <div className='ui unstackable items'>
         {productComponents}
-        </div>
-        );
+      </div>
+    );
   }
 }
 
 export default ProductList
+
+
 
